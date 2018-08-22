39af78076a47496291cd7b9d21f954d95316a598
codayblue <codayblue@gmail.com>
2018-08-22 16:18:38 -0700

    Altered the start up script to check if installed via composer
    - Setup a check to see if autoload is there.
    - Pull in autoload if found to autoload net_gearman

--- bin/pear_manager.php
+++ bin/pear_manager.php
@@ -6,6 +6,10 @@
  */
 define("NET_GEARMAN_JOB_CLASS_PREFIX", "");
 
+if (file_exists(__DIR__ . '/../../../autoload.php')) {
+    require __DIR__ . '/../../../autoload.php';
+}
+
 if(!class_exists("GearmanManager\Bridge\GearmanPearManager")) {
     require __DIR__."/../src/Bridge/GearmanPearManager.php";
 }
